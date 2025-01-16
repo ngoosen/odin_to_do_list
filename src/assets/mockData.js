03cd8edba99bf590b8003c23380e591474199798
Noémie Goosen <noemie.goosen@gmail.com>
2025-01-16 14:16:15 +0100

    feat: add mock data

---
+++ src/assets/mockData.js
@@ -0,0 +1,33 @@
+export const mockTasks = [
+  {
+    project: "Mom's birthday",
+    mainTask: "Bake the cake",
+    subtasks: [
+      "Choose filling",
+      "Buy flour"
+    ],
+  },
+  {
+    project: "Mom's birthday",
+    mainTask: "Buy gift",
+    subtasks: [],
+  },
+  {
+    project: "Chores",
+    mainTask: "Laundry",
+    subtasks: [
+      "Fold laundry",
+      "Dry clothes",
+      "Start washing machine"
+    ],
+  },
+  {
+    project: "Work",
+    mainTask: "Prepare the meeting",
+    subtasks: [
+      "Call Bill",
+      "Finish presentation",
+      "Check room reservation"
+    ],
+  },
+];
